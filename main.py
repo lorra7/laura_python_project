f639a4dae9a804d7caf2b52d535d765644977685
Laura-baratier <laura.baratierpro@gmail.com>
2025-05-14 22:54:09 +0200

    modification of the hello function in file main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-def hello():
-    return "Hello, GitHub Actions!"
+def hello(name="GitHub Actions"):
+    return f"Hello, {name}!"
--- test_main.py
+++ test_main.py
@@ -1,3 +1,7 @@
 from main import hello
+
 def test_hello():
     assert hello() == "Hello, GitHub Actions!"
+
+def test_hello_custom_name():
+    assert hello("EPSI") == "Hello, EPSI!"
