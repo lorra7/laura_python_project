f33a340461a65adb99326541f9f787a1278c5aa2
Laura-baratier <laura.baratierpro@gmail.com>
2025-05-15 00:42:58 +0200

    Validation

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 def hello(firstname='John', lastname='Doe'):
-    if not isinstance(firstname, str, lastname, str):
-        raise TypeError("Le nom doit être une chaîne")    
-    return f"Hello, firstname lastname !"
+    if not isinstance(firstname, str): 
+        raise TypeError("Le prénom doit être une chaîne")
+    
+    if not isinstance(lastname, str): 
+        raise TypeError("Le nom doit être une chaîne")
+
+    if firstname is None:
+        raise TypeError("Il manque le prénom")
+
+    if lastname is None:
+        raise TypeError("Il manque le nom de famille")
+
+    return f"Hello, {firstname} {lastname}!"
+
